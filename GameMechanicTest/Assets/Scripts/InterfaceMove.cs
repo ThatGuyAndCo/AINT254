f87f7f089ebc8c576b902c14c0f725b8ff457d66
ThatGuyAndCo <jollypolly889@gmail.com>
2017-10-24 17:31:19 +0100

    Added an abstract for the character move functions

---
+++ GameMechanicTest/Assets/Scripts/InterfaceMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InterfaceMove : MonoBehaviour {
+
+	Vector3 c_startPos;
+	Vector3 c_endPos;
+
+	protected abstract void CalculateMove (Vector3 l_startPos, Vector3 l_endPos);
+
+	protected abstract void CalculatePath(Vector3 l_startPos, Vector3 l_endPos);
+
+	protected abstract bool IsThereObstruction (Vector3[] l_path);
+
+	protected abstract void MoveToNextNode(Vector3 l_currentNode, Vector3 l_nextNode);
+
+	protected abstract bool IsTurnOver(Vector3 l_startPos, Vector3 l_endPos);
+
+	public abstract string InitiateMove(Vector3 l_startPos, Vector3 l_endPos, bool l_playerAttacked);
+}
